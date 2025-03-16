a8207f9edea1c739188bf1c6f057887390d340d4
FacundoBeloqui <fbeloqui@fi.uba.ar>
2025-03-16 15:44:06 -0300

    Terminado!

--- TPs/tp0/main.go
+++ TPs/tp0/main.go
@@ -1,12 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"tp0/ejercicios"
 )
 
+const ruta1 = "archivo1.in"
+const ruta2 = "archivo2.in"
+
+func leerArchivo(ruta string) []int {
+	var vectorOrdenado []int
+	archivo, err := os.Open(ruta)
+	if err != nil {
+		fmt.Printf("Error %v al abrir el archivo %s", ruta, err)
+	}
+	defer archivo.Close()
+
+	s := bufio.NewScanner(archivo)
+	for s.Scan() {
+		numero, e := strconv.Atoi(s.Text())
+		if e == nil {
+			vectorOrdenado = append(vectorOrdenado, numero)
+		}
+	}
+	err = s.Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return vectorOrdenado
+}
+
 func main() {
-	vector1 := []int{4, 2, 3, 4}
-	maximo := ejercicios.Maximo(vector1)
-	fmt.Printf("%d", maximo)
+	vector1 := leerArchivo(ruta1)
+	vector2 := leerArchivo(ruta2)
+	comparacion := ejercicios.Comparar(vector1, vector2)
+	if comparacion == 1 {
+		ejercicios.Seleccion(vector1)
+		for i := 0; i < len(vector1); i++ {
+			fmt.Printf("%d\n", vector1[i])
+		}
+
+	} else if comparacion == -1 {
+		ejercicios.Seleccion(vector2)
+		for i := 0; i < len(vector2); i++ {
+			fmt.Printf("%d\n", vector2[i])
+		}
+	}
 }

a56241da337702d65c3b0cf5ce4561ae55fdf24e
FacundoBeloqui <fbeloqui@fi.uba.ar>
2025-04-01 12:49:54 -0300

    Ahora si. TDA pila terminado

--- tdas/pila/pila_dinamica.go
+++ tdas/pila/pila_dinamica.go
@@ -1,14 +1,26 @@
 package pila
 
+const CAPACIDAD_INICIAL = 1
+const DOBLE = 2
+const MITAD = 2
+const CUADRUPLE = 4
+
 /* Definición del struct pila proporcionado por la cátedra. */
+
 type pilaDinamica[T any] struct {
-	datos     []T
-	cantidad  int
-	capacidad int
+	datos    []T
+	cantidad int
 }
 
-func redimensionar[T any](p *pilaDinamica[T]) {
-	nuevosDatos := make([]T, p.capacidad)
+func CrearPilaDinamica[T any]() Pila[T] {
+	return &pilaDinamica[T]{
+		datos:    make([]T, CAPACIDAD_INICIAL),
+		cantidad: 0,
+	}
+}
+
+func redimensionar[T any](p *pilaDinamica[T], nuevaCapacidad int) {
+	nuevosDatos := make([]T, nuevaCapacidad)
 	copy(nuevosDatos, p.datos)
 	p.datos = nuevosDatos
 }
@@ -26,33 +38,20 @@ func (p *pilaDinamica[T]) VerTope() T {
 }
 
 func (p *pilaDinamica[T]) Apilar(t T) {
-	const DOBLE = 2
-	if p.cantidad == p.capacidad {
-		p.capacidad *= DOBLE
-		redimensionar(p)
+	if p.cantidad == len(p.datos) {
+		redimensionar(p, len(p.datos)*DOBLE)
 	}
 	p.datos[p.cantidad] = t
 	p.cantidad++
 }
 
 func (p *pilaDinamica[T]) Desapilar() T {
-	const MITAD = 2
-	const CUADRUPLE = 4
 	if p.EstaVacia() {
 		panic("La pila esta vacia")
 	}
-	if p.cantidad*CUADRUPLE <= p.capacidad {
-		p.capacidad /= MITAD
-		redimensionar(p)
+	if p.cantidad*CUADRUPLE <= len(p.datos) {
+		redimensionar(p, len(p.datos)/MITAD)
 	}
 	p.cantidad--
 	return p.datos[p.cantidad]
-}
-
-func CrearPilaDinamica[T any]() Pila[T] {
-	return &pilaDinamica[T]{
-		datos:     make([]T, 1),
-		cantidad:  0,
-		capacidad: 1,
-	}
 }

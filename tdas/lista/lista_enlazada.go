b2b69205e874dbfb8cf5f56c6e610a8b30745bc6
FacundoBeloqui <fbeloqui@fi.uba.ar>
2025-04-22 16:46:50 -0300

    Terminadas funciones facu

--- tdas/lista/lista_enlazada.go
+++ tdas/lista/lista_enlazada.go
@@ -11,6 +11,12 @@ type listaEnlazada[T any] struct {
 	largo   int
 }
 
+type iterListaEnlazada[T any] struct {
+	actual   *nodoLista[T]
+	anterior *nodoLista[T]
+	lista    *listaEnlazada[T]
+}
+
 func crearNuevoNodo[T any](elemento T) *nodoLista[T] {
 	return &nodoLista[T]{
 		dato:      elemento,
@@ -26,38 +32,59 @@ func CrearListaEnlazada[T any]() Lista[T] {
 	}
 }
 func (l listaEnlazada[T]) EstaVacia() bool {
-	//TODO implement me
-	panic("implement me")
+	return l.largo == 0
 }
 
-func (l listaEnlazada[T]) InsertarPrimero(t T) {
-	//TODO implement me
-	panic("implement me")
+func (l *listaEnlazada[T]) InsertarPrimero(t T) {
+	nuevoNodo := crearNuevoNodo(t)
+	if l.EstaVacia() {
+		l.ultimo = nuevoNodo
+	} else {
+		nuevoNodo.siguiente = l.primero
+	}
+	l.primero = nuevoNodo
+	l.largo++
 }
 
-func (l listaEnlazada[T]) InsertarUltimo(t T) {
-	//TODO implement me
-	panic("implement me")
+func (l *listaEnlazada[T]) InsertarUltimo(t T) {
+	nuevoNodo := crearNuevoNodo(t)
+	if l.EstaVacia() {
+		l.primero = nuevoNodo
+	} else {
+		l.ultimo.siguiente = nuevoNodo
+	}
+	l.ultimo = nuevoNodo
+	l.largo++
 }
 
-func (l listaEnlazada[T]) BorrarPrimero() T {
-	//TODO implement me
-	panic("implement me")
+func (l *listaEnlazada[T]) BorrarPrimero() T {
+	if l.EstaVacia() {
+		panic("La lista esta vacia")
+	}
+	valor := l.primero.dato
+	l.primero = l.primero.siguiente
+	if l.primero == nil {
+		l.ultimo = nil
+	}
+	return valor
 }
 
-func (l listaEnlazada[T]) VerPrimero() T {
-	//TODO implement me
-	panic("implement me")
+func (l *listaEnlazada[T]) VerPrimero() T {
+	if l.EstaVacia() {
+		panic("La lista esta vacia")
+	}
+	return l.primero.dato
 }
 
-func (l listaEnlazada[T]) VerUltimo() T {
-	//TODO implement me
-	panic("implement me")
+func (l *listaEnlazada[T]) VerUltimo() T {
+	if l.EstaVacia() {
+		panic("La lista esta vacia")
+	}
+	return l.ultimo.dato
 }
 
-func (l listaEnlazada[T]) Largo() int {
-	//TODO implement me
-	panic("implement me")
+func (l *listaEnlazada[T]) Largo() int {
+	return l.largo
 }
 
 func (l listaEnlazada[T]) Iterar(visitar func(T) bool) {
@@ -68,4 +95,57 @@ func (l listaEnlazada[T]) Iterar(visitar func(T) bool) {
 func (l listaEnlazada[T]) Iterador() IteradorLista[T] {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (i *iterListaEnlazada[T]) VerActual() T {
+	if i.actual == nil {
+		panic("El iterador ya termino de recorrer")
+	} else {
+		return i.actual.dato
+	}
+}
+func (i *iterListaEnlazada[T]) HaySiguiente() bool {
+	return i.actual != nil
+}
+func (i *iterListaEnlazada[T]) Siguiente() {
+	if i.actual == nil {
+		panic("El iterador ya termino de recorrer")
+	} else {
+		i.anterior = i.actual
+		i.actual = i.actual.siguiente
+	}
+}
+
+func (i *iterListaEnlazada[T]) Insertar(elemento T) {
+	nuevoNodo := crearNuevoNodo(elemento)
+	if i.actual == i.lista.primero {
+		i.lista.primero = nuevoNodo
+
+	} else if i.actual == nil {
+		i.lista.ultimo = nuevoNodo
+
+	} else {
+		i.anterior = nuevoNodo
+	}
+	nuevoNodo.siguiente = i.actual
+	i.actual = nuevoNodo
+	i.lista.largo++
+}
+
+func (i *iterListaEnlazada[T]) Borrar() T {
+	if i.actual == nil {
+		panic("No hay elemento para borrar")
+	}
+	dato := i.actual.dato
+	if i.anterior != nil {
+		i.actual = i.actual.siguiente
+	} else {
+		i.lista.primero = i.actual.siguiente
+	}
+	if i.actual == i.lista.ultimo {
+		i.lista.ultimo = i.anterior
+	}
+	i.actual = i.actual.siguiente
+	i.lista.largo--
+	return dato
 }

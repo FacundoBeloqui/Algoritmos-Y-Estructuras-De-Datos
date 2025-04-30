682a367931bd7e37a17504500389a0e3932801a5
FacundoBeloqui <fbeloqui@fi.uba.ar>
2025-04-29 21:50:26 -0300

    quedan iteradores

--- tdas/diccionario/hash.go
+++ tdas/diccionario/hash.go
@@ -7,27 +7,62 @@ import (
 
 const TAMAÑO = 17
 
-type parClaveValor[K comparable, V any] struct {
+type ParClaveValor[K comparable, V any] struct {
 	clave K
 	dato  V
 }
 type hashAbierto[K comparable, V any] struct {
-	tabla    []TDALista.Lista[parClaveValor[K, V]]
+	tabla    []TDALista.Lista[ParClaveValor[K, V]]
 	tam      int
 	cantidad int
 }
 
 func CrearHash[K comparable, V any]() Diccionario[K, V] {
+	tabla := make([]TDALista.Lista[ParClaveValor[K, V]], TAMAÑO)
+	for i := range tabla {
+		tabla[i] = TDALista.CrearListaEnlazada[ParClaveValor[K, V]]()
+	}
 	return &hashAbierto[K, V]{
-		make([]TDALista.Lista[parClaveValor[K, V]], TAMAÑO),
+		tabla,
 		TAMAÑO,
 		0,
 	}
 }
 
-func (h hashAbierto[K, V]) Guardar(clave K, dato V) {
-	//TODO implement me
-	panic("implement me")
+func convertirABytes[K comparable](clave K) []byte {
+	return []byte(fmt.Sprintf("%v", clave))
+}
+
+func Hash[K comparable](clave K) uint32 {
+	data := convertirABytes(clave)
+	var hash uint32 = 0
+	for _, b := range data {
+		hash += uint32(b)
+		hash += hash << 10
+		hash ^= hash >> 6
+	}
+	hash += hash << 3
+	hash ^= hash >> 11
+	hash += hash << 15
+	return hash
+}
+
+func (h *hashAbierto[K, V]) Guardar(clave K, dato V) {
+	celda := Hash(clave) % TAMAÑO
+	iterador := h.tabla[celda].Iterador()
+	if h.Pertenece(clave) {
+		for iterador.HaySiguiente() {
+			if iterador.VerActual().clave == clave {
+				iterador.Borrar()
+				iterador.Insertar(ParClaveValor[K, V]{clave, dato})
+			}
+			iterador.Siguiente()
+		}
+	} else {
+		h.tabla[celda].InsertarUltimo(ParClaveValor[K, V]{clave, dato})
+	}
+	h.cantidad++
+
 }
 
 func (h hashAbierto[K, V]) Pertenece(clave K) bool {
@@ -58,24 +93,6 @@ func (h hashAbierto[K, V]) Iterar(f func(clave K, dato V) bool) {
 func (h hashAbierto[K, V]) Iterador() IterDiccionario[K, V] {
 	//TODO implement me
 	panic("implement me")
-}
-
-func convertirABytes[K comparable](clave K) []byte {
-	return []byte(fmt.Sprintf("%v", clave))
-}
-
-func Hash[K comparable](clave K) uint32 {
-	data := convertirABytes(clave)
-	var hash uint32 = 0
-	for _, b := range data {
-		hash += uint32(b)
-		hash += hash << 10
-		hash ^= hash >> 6
-	}
-	hash += hash << 3
-	hash ^= hash >> 11
-	hash += hash << 15
-	return hash
 }
 
 //primitivas del iterador
